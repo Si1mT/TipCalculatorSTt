6bf11ea61c8165e10df54784afdc8e8a34cba66c
Si1mT <33309403+Si1mT@users.noreply.github.com>
2018-09-27 07:41:05 +0300

    finished

---
+++ TipCalculator/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace TipCalculator
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView tipPercent;
+        TextView tipCost;
+        EditText baseBill;
+        TextView totalBill;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            SeekBar Seekbar = FindViewById<SeekBar>(Resource.Id.Seekbar_Tip);
+            Seekbar.ProgressChanged += SeekBar_ProgressChanged;
+
+            tipPercent = FindViewById<TextView>(Resource.Id.TextView_TipPercent);
+            tipCost = FindViewById<TextView>(Resource.Id.TextView_TipCostNumber);
+            
+            baseBill = FindViewById<EditText>(Resource.Id.EditText_Bill);
+            totalBill = FindViewById<TextView>(Resource.Id.TextView_TotalNumber);
+        }
+
+        public void SeekBar_ProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e)
+        {
+            tipPercent.Text = e.Progress.ToString();
+            int tipAmount = int.Parse(tipPercent.Text) * int.Parse(baseBill.Text) / 100;
+            tipCost.Text = tipAmount.ToString();
+            int total = int.Parse(tipCost.Text) + int.Parse(baseBill.Text);
+            totalBill.Text = total.ToString();
+        }
+    }
+}
